452ae3f5a01bfd9ee66461a7a06f16ead6a5f3ac
James Bellomy <jay.bells12@gmail.com>
2022-11-30 17:24:10 -0800

    Create game loop functions
    New functions were created, getPlayerChoice, getComputerChoice,
    and playRound. Currently all functions return strings.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
-console.log("hello world")
+function getComputerChoice(): string {
+  // Use random number, 0-2 inclusive, to select a string to return, from the static array
+  return ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)]
+}
+
+function getPlayerChoice(): string {
+  let input: string | null;
+  let keepGoing: boolean = true;
+  let choice: string = "";
+  // Loop prompting user for a valid input. Loop continues until a valid response is entered, then the formatted response is returned
+  do{
+    input = prompt("Please type your choice: Rock, Paper, or Scissors?")
+    if(typeof input === "string") {
+      choice = input.toLowerCase();
+      if(choice === 'rock' || choice === 'paper' || choice === 'scissors') keepGoing = false;
+    }
+  }while(keepGoing)
+
+  // Formats string so first letter is capitalized
+  return choice.charAt(0).toUpperCase() + choice.slice(1);
+}
+
+
+//playRound function compares player selection to computer selection and outputs a string declaring if player is winner or loser and the choices made for the round.
+function playRound(playerSelection: string, computerSelection: string): string {
+  let msg: string = "";
+  
+  if(playerSelection === computerSelection) {
+    msg = "It's a tie!"
+  }else{
+    switch(playerSelection){
+      case "Rock":
+        if(computerSelection === "Scissors"){
+          msg = `You Win! ${playerSelection} crushes ${computerSelection}`
+        }else{
+          msg = `You Lose! ${playerSelection} is covered by ${computerSelection}`
+        }
+        break;
+      case "Paper":
+        if(computerSelection === "Rock"){
+          msg = `You Win! ${playerSelection} covers ${computerSelection}`
+        }else{
+          msg = `You Lose! ${playerSelection} is cut by ${computerSelection}`
+        }
+        break;
+      case "Scissors":
+        if(computerSelection === "Paper"){
+          msg = `You Win! ${playerSelection} cuts ${computerSelection}`
+        }else{
+          msg = `You Lose! ${playerSelection} is crushed by ${computerSelection}`
+        }
+        break;
+    }
+  }
+
+  return msg;
+}
