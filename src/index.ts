5ec427a6d6f4d6f1cdbb09cdb1f78ca0ef10c053
James Bellomy <jay.bells12@gmail.com>
2022-12-14 18:19:07 -0800

    Create game logic
    Implemented basic game logic for a visual based interface. Also made
    some slight modifications to styling and the html structure.

--- src/index.ts
+++ src/index.ts
@@ -1,87 +1,255 @@
+interface ScoreCard {
+  player: number;
+  computer: number;
+}
+
+function gameLoop(): void {
+  //All necessary DOM queries are executed once, here, on initialization.
+  const choices: HTMLCollection = document.getElementsByClassName("choice");
+  const resultsElement: HTMLElement | null = document.getElementById("results");
+  const overlayElements: HTMLCollection | null =
+    document.getElementsByClassName("overlay");
+  const playerScore: HTMLElement | null =
+    document.getElementById("player_score");
+  const computerScore: HTMLElement | null =
+    document.getElementById("computer_score");
+  const hands: HTMLCollection | null =
+    document.getElementsByClassName("selection--wrapper");
+  const playerHand: Element | null = hands[0].firstElementChild;
+  const computerHand: Element | null = hands[1].firstElementChild;
+  const scores: ScoreCard = {
+    player: 0,
+    computer: 0,
+  };
+  let throttled: boolean = false;
+
+  // The player click starts and progresses the game loop.
+  function playRound(e: Event): void {
+    if (throttled) return;
+    throttled = true;
+
+    const playerChoice: EventTarget | null = e.target;
+
+    try {
+      if (!(playerChoice instanceof HTMLElement))
+        throw new Error("Click target is not an HTML element");
+      if (!playerChoice.dataset.value)
+        throw new Error(
+          "Element clicked does not have a data attribute of value"
+        );
+      if (!hands) throw new Error("Could not find game hands elements");
+      if (!playerHand || !computerHand)
+        throw new Error("Could not find individual hand element");
+      if (!resultsElement) throw new Error("Could not find results element");
+      if (!overlayElements) throw new Error("Could not find overlay elements");
+      if (!playerScore) throw new Error("Could not find player score element");
+      if (!computerScore)
+        throw new Error("Could not find computer score element");
+
+      const playerInput: string = playerChoice.dataset.value;
+      const computerInput: string = getComputerChoice();
+      const outcome: string = calcWinner(playerInput, computerInput);
+
+      insertHandSelections(playerHand, "rock", computerHand, "rock");
+      animateHands(hands);
+      // Timeout here to change the image of the hands just before the animation finishes,
+      // this gives a more natural look and feel to the transition. animation length is currently 1600ms
+      setTimeout(
+        () =>
+          insertHandSelections(
+            playerHand,
+            playerInput,
+            computerHand,
+            computerInput
+          ),
+        1550
+      );
+
+      // Timeout here to allow the animation to finish and half a second to pass for the player to see the hands of the game board,
+      // then the outcome is displayed in text, while everything else is faded. The user must click to continue playing.
+      // Timings to manipulate:
+      //      animation length: (1600) + delay(600)
+      //      transition duration: 250ms
+      setTimeout(() => {
+        nextRound(outcome, hands, scores, playerScore, computerScore);
+        isGameOver(
+          resultsElement,
+          overlayElements,
+          scores,
+          playerScore,
+          computerScore
+        );
+        throttled = false;
+      }, 2200);
+    } catch (err: any) {
+      console.log(`Error: ${err}`);
+    }
+  }
+
+  for (let i: number = 0; i < choices.length; i++) {
+    choices[i].addEventListener("click", playRound);
+  }
+}
+
 function getComputerChoice(): string {
   // Use random number, 0-2 inclusive, to select a string to return, from the static array
-  return ["Rock", "Paper", "Scissors"][Math.floor(Math.random() * 3)];
+  return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
 }
 
-function getPlayerChoice(): string {
-  let input: string | null;
-  let keepGoing: boolean = true;
-  let choice: string = "";
-  // Loop prompting user for a valid input. Loop continues until a valid response is entered, then the formatted response is returned
-  do {
-    input = prompt("Please type your choice: Rock, Paper, or Scissors?");
-    if (typeof input === "string") {
-      choice = input.toLowerCase();
-      if (choice === "rock" || choice === "paper" || choice === "scissors")
-        keepGoing = false;
-    }
-  } while (keepGoing);
-
-  // Formats string so first letter is capitalized
-  return choice.charAt(0).toUpperCase() + choice.slice(1);
+function calcWinner(player: string, computer: string): string {
+  if (player === computer) return "Draw";
+  switch (player) {
+    case "rock":
+      if (computer === "scissors") {
+        return "Win";
+      } else {
+        return "Lose";
+      }
+    case "paper":
+      if (computer === "rock") {
+        return "Win";
+      } else {
+        return "Lose";
+      }
+    case "scissors":
+      if (computer === "paper") {
+        return "Win";
+      } else {
+        return "Lose";
+      }
+    default:
+      throw new Error("Invalid player input string");
+  }
 }
 
-//playRound function compares player selection to computer selection in the form of strings, it returns a win/lose message.
-function playRound(
-  playerSelection: string,
-  computerSelection: string,
-  scoreCard: [number, number]
-): string {
-  let msg: string = "";
+function animateHands(hands: HTMLCollection): void {
+  for (let i = 0; i < hands.length; i++) {
+    hands[i].classList.add("animateHand");
+  }
+}
 
-  if (playerSelection === computerSelection) {
-    msg = "It's a tie!";
-  } else {
-    switch (playerSelection) {
-      case "Rock":
-        if (computerSelection === "Scissors") {
-          msg = `You Win! ${playerSelection} crushes ${computerSelection}`;
-          scoreCard[0]++;
-        } else {
-          msg = `You Lose! ${playerSelection} is covered by ${computerSelection}`;
-          scoreCard[1]++;
-        }
-        break;
-      case "Paper":
-        if (computerSelection === "Rock") {
-          msg = `You Win! ${playerSelection} covers ${computerSelection}`;
-          scoreCard[0]++;
-        } else {
-          msg = `You Lose! ${playerSelection} is cut by ${computerSelection}`;
-          scoreCard[1]++;
-        }
-        break;
-      case "Scissors":
-        if (computerSelection === "Paper") {
-          msg = `You Win! ${playerSelection} cuts ${computerSelection}`;
-          scoreCard[0]++;
-        } else {
-          msg = `You Lose! ${playerSelection} is crushed by ${computerSelection}`;
-          scoreCard[1]++;
-        }
-        break;
-    }
+function insertHandSelections(
+  pHand: Element,
+  pSelection: string,
+  cHand: Element,
+  cSelection: string
+): void {
+  if (!pHand.hasAttribute("src") || !cHand.hasAttribute("src"))
+    throw new Error("No src attribute found on selection element");
+
+  pHand.setAttribute("src", `./assets/${pSelection}-player.svg`);
+  cHand.setAttribute("src", `./assets/${cSelection}-comp.svg`);
+}
+
+function updateScoreboard(
+  scores: ScoreCard,
+  playerScore: HTMLElement,
+  computerScore: HTMLElement
+): void {
+  playerScore.innerText = `${scores.player}`;
+  computerScore.innerText = `${scores.computer}`;
+}
+
+function activateOverlay(
+  resultsElement: HTMLElement,
+  overlayElements: HTMLCollection,
+  resultText: string,
+  scores: ScoreCard,
+  playerScore: HTMLElement,
+  computerScore: HTMLElement
+): void {
+  for (let i = 0; i < overlayElements.length; i++) {
+    overlayElements[i].classList.add("fade");
   }
 
-  return msg;
+  resultsElement.innerText = resultText;
+  window.addEventListener("click", (e: Event) =>
+    resetGame(
+      e,
+      scores,
+      playerScore,
+      computerScore,
+      overlayElements,
+      resultsElement
+    )
+  );
 }
 
-// Main game loop: currently hardcoded to run until either the player or the computer reaches 5 wins.
+function nextRound(
+  resultsString: string,
+  hands: HTMLCollection,
+  scores: ScoreCard,
+  playerScore: HTMLElement,
+  computerScore: HTMLElement
+): void {
+  // Can insert logic here for game reset, IE reaching 5 points
+  if (resultsString === "Win") {
+    scores.player += 1;
+  } else if (resultsString === "Lose") {
+    scores.computer += 1;
+  }
 
-function game(): void {
-  const scoreCard: [number, number] = [0, 0];
+  // for (let i = 0; i < overlayElements.length; i++) {
+  //   overlayElements[i].classList.remove("fade");
+  // }
 
-  do {
-    console.log(playRound(getPlayerChoice(), getComputerChoice(), scoreCard));
-    console.log(
-      `Current score: Player - ${scoreCard[0]} Computer - ${scoreCard[1]}`
+  for (let i = 0; i < hands.length; i++) {
+    hands[i].classList.remove("animateHand");
+  }
+
+  // resultsElement.innerText = "";
+
+  updateScoreboard(scores, playerScore, computerScore);
+  // insertHandSelections(pHand, "rock", cHand, "rock");
+}
+
+function isGameOver(
+  resultsElement: HTMLElement,
+  overlayElements: HTMLCollection,
+  scores: ScoreCard,
+  playerScore: HTMLElement,
+  computerScore: HTMLElement
+): void {
+  if (scores.player === 5) {
+    activateOverlay(
+      resultsElement,
+      overlayElements,
+      "Congratulations! You won!",
+      scores,
+      playerScore,
+      computerScore
     );
-  } while (scoreCard[0] < 5 && scoreCard[1] < 5);
-
-  //The player score is held in first position
-  if (scoreCard[0] >= 5) {
-    console.log(`Great Job! You Won!!`);
-  } else {
-    console.log(`Aw shucks! The computer won. Better luck next time!`);
+  } else if (scores.computer === 5) {
+    activateOverlay(
+      resultsElement,
+      overlayElements,
+      "Better luck next time!",
+      scores,
+      playerScore,
+      computerScore
+    );
   }
 }
+
+function resetGame(
+  e: Event,
+  scores: ScoreCard,
+  pScoreElement: HTMLElement,
+  cScoreElement: HTMLElement,
+  overlayElements: HTMLCollection,
+  resultsElement: HTMLElement
+): void {
+  e.stopPropagation();
+
+  scores.player = 0;
+  scores.computer = 0;
+
+  for (let i = 0; i < overlayElements.length; i++) {
+    overlayElements[i].classList.remove("fade");
+  }
+
+  resultsElement.innerText = "";
+  updateScoreboard(scores, pScoreElement, cScoreElement);
+}
+// Initialize
+gameLoop();
